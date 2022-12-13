572ce95dfb3253eff2c164a39c02fc46ffe39024
fasilmsageer <fasilmsageer@gmail.com>
2022-12-13 14:25:34 +0530

    navbar

---
+++ src/app/employee-login/employee-login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-employee-login',
+  templateUrl: './employee-login.component.html',
+  styleUrls: ['./employee-login.component.css']
+})
+export class EmployeeLoginComponent {
+
+  constructor(private api:ApiService){}
+
+  empusername=""
+  emppassword=""
+
+  readValue=()=>
+  {
+    if (this.empusername=="" && this.emppassword==)
+  }
+
+}
